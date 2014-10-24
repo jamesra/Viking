6d579fef7b8f5731cff1718dc9e1d976a510be5b
James Anderson <james.r.andreson@utah.edu>
2014-10-24 15:50:13 -0700

    Fixed equality with null.  Added Equality overrides to LocationLink

--- Clients/WebAnnotationModel/Objects/LocationLinkObj.cs
+++ Clients/WebAnnotationModel/Objects/LocationLinkObj.cs
@@ -35,7 +35,7 @@ namespace WebAnnotationModel
 
             LocationLinkKey other = (LocationLinkKey)obj;
 
-            return this == other; 
+            return (A == other.A) && (B == other.B);
         }
 
         public override string ToString()
@@ -50,11 +50,25 @@ namespace WebAnnotationModel
 
         public static bool operator ==(LocationLinkKey A, LocationLinkKey B)
         {
+            return object.Equals(A, B);
+
+            if (object.Equals(A, B))
+                return true;
+            if (object.Equals(A, null) || object.Equals(B, null))
+                return false; 
+
             return (A.A == B.A) && (A.B == B.B);
         }
 
         public static bool operator !=(LocationLinkKey A, LocationLinkKey B)
         {
+            return !object.Equals(A, B);
+
+            if (object.Equals(A, B))
+                return false;
+            if (object.Equals(A, null) || object.Equals(B, null))
+                return true; 
+
             return !((A.A == B.A) && (A.B == B.B));
         }
 
@@ -72,12 +86,70 @@ namespace WebAnnotationModel
         public override LocationLinkKey ID
         {
             get { return new LocationLinkKey(this); }
+            internal set { 
+                  Data.SourceID = value.A;
+                  Data.TargetID = value.B;
+            }
+        }
+
+        public override string ToString()
+        {
+            return ID.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            if (!this.GetType().IsInstanceOfType(obj))
+                return false;
+
+            LocationLinkObj other = (LocationLinkObj)obj;
+
+            return (A == other.A) && (B == other.B);
         }
 
         protected override int GenerateHashCode()
         {
-            return (int)(Data.SourceID % int.MaxValue);
+            return ID.GetHashCode(); 
         }
+
+        public override int GetHashCode()
+        {
+            return ID.GetHashCode();
+        }
+
+        public static bool operator ==(LocationLinkObj A, LocationLinkObj B)
+        {
+            return object.Equals(A, B);
+
+            if (object.Equals(A, B))
+                return true;
+            if (object.Equals(A, null) || object.Equals(B, null))
+                return false;  
+
+            return (A.ID == B.ID);
+        }
+
+        public static bool operator !=(LocationLinkObj A, LocationLinkObj B)
+        {
+            return !object.Equals(A, B);
+
+            if (object.Equals(A, B))
+                return false;
+            if(object.Equals(A,null) || object.Equals(B,null))                
+                return true; 
+
+            return !((A.ID == B.ID));
+        }
+
+        public int CompareTo(LocationLinkKey other)
+        {
+            if (A != other.A)
+                return (int)(other.A - A);
+            else
+                return (int)(other.B - B);
+        }   
 
         public long A
         {
@@ -98,9 +170,10 @@ namespace WebAnnotationModel
         public LocationLinkObj(long IDA,
                                long IDB)
         {
+            Debug.Assert(IDA != IDB);
             LocationLink link = new LocationLink();
-            link.SourceID = IDA;
-            link.TargetID = IDB;
+            link.SourceID = IDA < IDB ? IDA : IDB;
+            link.TargetID = IDA < IDB ? IDB : IDA;
             this.Data = link; 
         }
 
