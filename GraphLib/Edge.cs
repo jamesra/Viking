4132752fb63ed41d8ab56f6e3baa9d44672915a4
James Anderson <james.r.anderson@utah.edu>
2014-11-13 15:35:55 -0700

    Added missing package

---
+++ GraphLib/Edge.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics; 
+
+namespace GraphLib
+{ 
+    public class Edge<NODEKEY> : IComparer<Edge<NODEKEY>>, IComparable<Edge<NODEKEY>>
+        where NODEKEY : IComparable<NODEKEY>
+    {
+        public NODEKEY SourceNodeKey { get; private set; }
+        public NODEKEY TargetNodeKey { get; private set; }
+
+        public float Weight = 1;
+
+        public Edge(NODEKEY SourceNode, NODEKEY TargetNode)
+        {
+            Debug.Assert(SourceNode != null);
+            Debug.Assert(TargetNode != null);
+            this.SourceNodeKey = SourceNode;
+            this.TargetNodeKey = TargetNode;
+        }
+
+        public int Compare(Edge<NODEKEY> x, Edge<NODEKEY> y)
+        {
+            if (x == null && y == null)
+                return 0;
+            if (x == null)
+                return -1;
+            if (y == null)
+                return 1;
+
+            return x.CompareTo(y);
+        }
+
+        public int CompareTo(Edge<NODEKEY> other)
+        {
+            int SourceComparison = this.SourceNodeKey.CompareTo(other.SourceNodeKey);
+            int TargetComparison = this.TargetNodeKey.CompareTo(other.TargetNodeKey);
+
+            if (SourceComparison == 0 && TargetComparison == 0)
+                return 0;
+
+            if (SourceComparison != 0)
+                return SourceComparison;
+
+            return TargetComparison;
+        }
+    }
+}
---
+++ GraphLib/Node.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GraphLib
+{
+    public class Node<KEY, EDGETYPE> : IComparer<Node<KEY, EDGETYPE>>, IComparable<Node<KEY, EDGETYPE>>
+        where KEY : IComparable<KEY>, IEquatable<KEY>
+        where EDGETYPE : Edge<KEY>
+    {
+        public KEY Key; 
+
+        /// <summary>
+        /// Keys are the ID of the other node in the edge, or our iD if it is a circular reference
+        /// </summary>
+        public SortedDictionary<KEY, List<EDGETYPE>> Edges = new SortedDictionary<KEY, List<EDGETYPE>>();
+
+        public Node(KEY k)
+        {
+            this.Key = k;
+        }
+
+        public void AddEdge(EDGETYPE Link)
+        { 
+            
+            KEY PartnerKey = Link.SourceNodeKey;
+            if (Link.SourceNodeKey.CompareTo(Link.TargetNodeKey) == 0)
+            {
+                //Circular reference, just proceed
+            }
+            else if (Link.SourceNodeKey.CompareTo(this.Key) == 0)
+            {
+                PartnerKey = Link.TargetNodeKey;
+            }
+
+            List<EDGETYPE> edgeList = null; 
+            if( Edges.ContainsKey(PartnerKey))
+            {
+                edgeList = Edges[PartnerKey]; 
+            }
+            else
+            {
+                edgeList = new List<EDGETYPE>();
+                Edges[PartnerKey] = edgeList; 
+            }
+                
+            edgeList.Add(Link); 
+            
+        }
+
+
+        public int Compare(Node<KEY, EDGETYPE> x, Node<KEY, EDGETYPE> y)
+        {
+            return this.CompareTo(y); 
+        }
+
+        public int CompareTo(Node<KEY, EDGETYPE> other)
+        {
+            return this.Key.CompareTo(other.Key);
+        }
+    }
+}
