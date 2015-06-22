bfead50630914ea148da428e2eb17918e8202201
James Anderson <james.r.andreson@utah.edu>
2015-06-22 14:31:19 -0700

    A more thorough deep delete structures command

--- Servers/AnnotationService/SQL/UtilityScripts/DeepDeleteStructure.sql
+++ Servers/AnnotationService/SQL/UtilityScripts/DeepDeleteStructure.sql
@@ -1,20 +1,32 @@
 DECLARE @DeleteID bigint
-Set @DeleteID = 449
+Set @DeleteID = 60806
+
+if OBJECT_ID('tempdb..#StructuresToDelete') is not null
+	DROP Table #StructuresToDelete
+
+select ID into #StructuresToDelete from (Select ID from Structure where ID = @DeleteID or ParentID = @DeleteID) as ID
 
 delete from LocationLink
 where A in 
 (
 Select ID from Location 
-where ParentID = @DeleteID ) 
+where ParentID in (Select ID From #StructuresToDelete) ) 
 
 delete from LocationLink
 where B in 
 (
-Select ID from Location 
-where ParentID = @DeleteID ) 
+Select ID from Location where ParentID in (Select ID From #StructuresToDelete) ) 
 
 delete from Location
+where ParentID in (Select ID From #StructuresToDelete)
+
+delete from StructureLink where SourceID in (Select ID From #StructuresToDelete) or TargetID in (Select ID From #StructuresToDelete)
+
+delete from Structure
 where ParentID=@DeleteID
 
 delete from Structure
 where ID=@DeleteID
+
+if OBJECT_ID('tempdb..#StructuresToDelete') is not null
+	DROP Table #StructuresToDelete
