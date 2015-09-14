1b48f6a547ec0400b4cd0eb9a5dfedc7af826925
James Anderson <james.r.andreson@utah.edu>
2015-09-14 11:48:09 -0700

    Conversion methods for GridRectangle to BoundingRectangle object used by annotation service

---
+++ Clients/WebAnnotationModel/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Geometry;
+using WebAnnotationModel.Service;
+
+
+namespace WebAnnotationModel
+{
+    public static class GeometryExtensions
+    {
+        public static GridRectangle ToGridRectangle(WebAnnotationModel.Service.BoundingRectangle bbox)
+        {
+            return new GridRectangle(bbox.XMin, bbox.XMax, bbox.YMin, bbox.YMax);
+        }
+
+        public static WebAnnotationModel.Service.BoundingRectangle ToGridRectangle(GridRectangle rect)
+        {
+            return new BoundingRectangle() { XMin = rect.Left, XMax = rect.Right, YMin = rect.Bottom, YMax = rect.Top };
+        }
+    }
+}
