4498a405755faab681c368bfa40268f8c812237b
James Anderson <james.r.andreson@utah.edu>
2015-09-29 14:41:51 -0700

    Update SelectSectionLocationsAndLinksInBounds

--- Servers/AnnotationService/SQL/DatabaseCreateUpdate/CreateUpdateDatabase.sql
+++ Servers/AnnotationService/SQL/DatabaseCreateUpdate/CreateUpdateDatabase.sql
@@ -2983,6 +2983,67 @@ end
 	end
 	go
 
+	if(not(exists(select (1) from DBVersion where DBVersionID = 32)))
+	begin
+     print N'Update spatial query functions'
+	 BEGIN TRANSACTION thirtytwo
+		 
+		 EXEC('
+			ALTER PROCEDURE [dbo].[SelectSectionLocationsAndLinksInBounds]
+				-- Add the parameters for the stored procedure here
+				@Z float,
+				@BBox geometry,
+				@Radius float,
+				@QueryDate datetime
+			AS
+			BEGIN
+				-- SET NOCOUNT ON added to prevent extra result sets from
+				-- interfering with SELECT statements.
+				SET NOCOUNT ON;
+
+				IF OBJECT_ID(''tempdb..#LocationsInBounds'') IS NOT NULL DROP TABLE #LocationsInBounds
+
+				--Selecting all columns once into LocationsInBounds and then selecting the temp table is a huge time saver.  3-4 seconds instead of 20.
+
+				select * into #LocationsInBounds FROM Location where Z = @Z AND (@BBox.STIntersects(VolumeShape) = 1) AND Radius >= @Radius order by ID
+	 
+				IF @QueryDate IS NOT NULL
+					Select * from #LocationsInBounds where LastModified >= @QueryDate
+				ELSE
+					Select * from #LocationsInBounds
+	 
+				IF @QueryDate IS NOT NULL
+					-- Insert statements for procedure here
+					Select * from LocationLink
+					 WHERE ((A in 
+					(select ID from #LocationsInBounds))
+					  OR
+					  (B in 
+					(select ID from #LocationsInBounds)))
+					 AND Created >= @QueryDate
+				ELSE
+					-- Insert statements for procedure here
+					Select * from LocationLink
+					 WHERE ((A in (select ID from #LocationsInBounds))
+							OR	
+							(B in (select ID from #LocationsInBounds)))
+	
+	 
+			END 
+		')
+
+		if(@@error <> 0)
+		 begin
+		   ROLLBACK TRANSACTION 
+		   RETURN
+		 end
+
+	 INSERT INTO DBVersion values (32, 
+		    N'Update spatial query functions'  ,getDate(),User_ID())
+	 COMMIT TRANSACTION thirtyone
+	end
+
+
 
 --from here on, continually add steps in the previous manner as needed.
 	COMMIT TRANSACTION main
