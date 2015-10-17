be5bdd2c39bfc58fa7db974a51b60700b09bdda4
Jamesan <james.r.anderson@utah.edu>
2015-10-16 20:15:34 -0400

    Add color conversion extension

---
+++ Clients/Viking/VikingXNAGraphics/Extensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VikingXNAGraphics
+{
+    public static class ColorExtensions
+    {
+        public static Microsoft.Xna.Framework.Color ToXNAColor(this System.Drawing.Color color)
+        {
+            return new Microsoft.Xna.Framework.Color((int)color.R,
+                                                    (int)color.G,
+                                                    (int)color.B,
+                                                    (int)color.A);
+        }
+
+        public static Microsoft.Xna.Framework.Color ToXNAColor(this System.Drawing.Color color, float alpha)
+        {
+            return new Microsoft.Xna.Framework.Color((int)color.R,
+                                                    (int)color.G,
+                                                    (int)color.B,
+                                                    alpha);
+        }
+    }
+}
+
