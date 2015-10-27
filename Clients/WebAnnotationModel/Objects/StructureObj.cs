49ca465aced9a0d945722703b9384bbdb184f4ed
James Anderson <james.r.andreson@utah.edu>
2015-10-27 10:51:40 -0700

    Added TypeID property

--- Clients/WebAnnotationModel/Objects/StructureObj.cs
+++ Clients/WebAnnotationModel/Objects/StructureObj.cs
@@ -35,6 +35,11 @@ namespace WebAnnotationModel
             }
         }
 
+        public long TypeID
+        {
+            get { return Data.TypeID; }
+        }
+
         /// <summary>
         /// The ID for newo bjects can change from a negative number to the number in the database.
         /// In this case make sure we always return the same hash code.  As a result this is called for each object only once.
