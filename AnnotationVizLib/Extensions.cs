f3e970a545c05c7e3c4cf71d352bb18dff6ffe57
James Anderson <james.r.anderson@utah.edu>
2015-11-14 23:15:43 -0800

    Added missing files

---
+++ AnnotationVizLib/Extensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AnnotationVizLib.AnnotationService;
+
+namespace AnnotationVizLib
+{
+    static class Extensions
+    {
+        public static List<ObjAttribute> Attributes(this Location loc)
+        {
+            return ObjAttribute.Parse(loc.AttributesXml);
+        }
+
+        public static bool IsVericosityCap(this Location loc)
+        {
+            List<ObjAttribute> attribs = loc.Attributes();
+            return attribs.Any(a => a.Name == "Vericosity Cap");
+        }
+
+        public static bool IsUntraceable(this Location loc)
+        {
+            List<ObjAttribute> attribs = loc.Attributes();
+            return attribs.Any(a => a.Name == "Untraceable");
+        }
+    }
+}
