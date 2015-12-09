b4443929cf637ee3430123a9aae05d8f1fa2db12
James Anderson <james.r.andreson@utah.edu>
2015-12-09 15:20:14 -0800

    Added missing files

---
+++ Clients/Viking/WebAnnotation/View/LocationCurveView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Geometry;
+using WebAnnotationModel;
+
+namespace WebAnnotation.View
+{
+    abstract class LocationCurveView : LocationLineView
+    {
+        public LocationCurveView(LocationObj obj) : base(obj) { }
+        public abstract GridVector2[] MosaicCurveControlPoints { get; }
+        public abstract GridVector2[] VolumeCurveControlPoints { get; }
+    }
+}
