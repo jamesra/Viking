85834525f352c1e1f058f2719fa84448b4174cd7
James Anderson <james.r.anderson@utah.edu>
2016-04-01 18:40:11 -0700

    Added tests for ConnectomeDataModel

---
+++ Servers/ConnectomeDataModelTests/StoredProcedureTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConnectomeDataModel;
+using System.Collections.Generic;
+
+namespace ConnectomeDataModelTests
+{
+    [TestClass]
+    public class TestConnectomeDataModel
+    {
+        [TestMethod]
+        public void TestSelectNetworkStructureIDs()
+        {
+            ConnectomeDataModel.ConnectomeEntities context = new ConnectomeEntities();
+
+            List<long> IDs = new List<long>(new long[] { 476, 514 });
+            IList<long> networkStructureIDs = context.SelectNetworkStructureIDs(IDs, 3);
+            System.Diagnostics.Trace.Write(networkStructureIDs.Count.ToString());
+
+        }
+
+        [TestMethod]
+        public void TestSelectNetworkDetails()
+        {
+            ConnectomeDataModel.ConnectomeEntities context = new ConnectomeEntities();
+
+            List<long> IDs = new List<long>(new long[] { 476, 514 });
+            NetworkDetails network = context.SelectNetworkDetails(IDs, 2);
+
+            Console.WriteLine($"Nodes = {network.Nodes.Length}");
+            Console.WriteLine($"Children = {network.ChildNodes.Length}");
+            Console.WriteLine($"Edges = {network.Edges.Length}");
+            Console.WriteLine("");
+
+            
+            Console.WriteLine($"Nodes = {network.Nodes[network.Nodes.Length-1].Label}");
+            Console.WriteLine($"Children = {network.ChildNodes[network.ChildNodes.Length - 1].ID}");
+            Console.WriteLine($"Edges = {network.Edges[network.Edges.Length-1].Bidirectional}"); 
+        }
+    }
+}
