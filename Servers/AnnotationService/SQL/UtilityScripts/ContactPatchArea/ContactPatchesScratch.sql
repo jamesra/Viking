958cab63157395f8ff45899a41e38906d8afd8ea
jamesra <james.r.anderson@utah.edu>
2016-05-25 14:08:31 -0600

    Corrected calculation of contact area.

--- Servers/AnnotationService/SQL/UtilityScripts/ContactPatchArea/ContactPatchesScratch.sql
+++ Servers/AnnotationService/SQL/UtilityScripts/ContactPatchArea/ContactPatchesScratch.sql
@@ -26,17 +26,17 @@ join Location T      ON L.TargetID = T.ParentID AND T.Z = S.Z
 order by S.ParentID, T.ParentID
 
 GO
---select * from #StructureLinks
 
 --Count the cases where we have two locations for the same structure on the same Z section that create extra fake contact patches.
 --NOTE: This code breaks if the Synapse overlaps with BOTH PSD pairs.  There's no fixing it.  Just ensure the synapse only overlaps with the correct PSD and not the more distant one.
-select SParentID, TParentID, Z, NumPairs
+/*
+select SParentID, TParentID, SZ, NumPairs
 into #ExtraStructureLinks
-from (Select SParentID, TParentID, Z, Count(SParentID) as NumPairs
+from (Select SParentID, TParentID, SZ, Count(SParentID) as NumPairs
 	  from #StructureLinks
-	  group by SParentID, TParentID, Z) as ExtraStructureLinks
+	  group by SParentID, TParentID, SZ) as ExtraStructureLinks
 where NumPairs > 1 order by SParentID
-
+ 
 select SL.SID, SL.TID, SMosaicShape.STDistance(TMosaicShape) as Distance
 into #PairDistance
 from #StructureLinks SL
@@ -46,6 +46,8 @@ select SL.SID, MIN(PD.Distance) as NearestPartnerDistance into #NearestPartner f
 join #ExtraStructureLinks EL ON SL.SParentID = EL.SParentID AND SL.TParentID = EL.TParentID AND SL.SZ = EL.SZ
 join #PairDistance PD ON PD.SID = SL.SID AND PD.TID = SL.TID
 group by SL.SID
+ 
+ */
 
 /*
 select SL.SID, SL.TID, PD.Distance into #PairsToDelete from #StructureLinks SL
@@ -54,16 +56,26 @@ inner join #NearestPartner NP ON NP.SID = SL.SID
 where PD.Distance > NP.NearestPartnerDistance
 */
 
+/*
 --Remove the locations that have a nearer paired location
 delete SL from #StructureLinks SL
 inner join #PairDistance PD ON PD.SID = SL.SID AND PD.TID = SL.TID
 inner join #NearestPartner NP ON NP.SID = SL.SID
 where PD.Distance > NP.NearestPartnerDistance
-
+*/
+/*
 --Validates we do not have extra pairs
-select SParentID, TParentID, SZ, NumPairs from (Select SParentID, TParentID, SZ, Count(SParentID) as NumPairs from #StructureLinks group by SParentID, TParentID, SZ) as ExtraStructureLinks where NumPairs > 1 order by NumPairs, SParentID
+select SParentID, TParentID, SZ, NumPairs from 
+	(
+		Select SParentID, TParentID, SZ, Count(SParentID) as NumPairs
+		 from #StructureLinks 
+		 group by SParentID, TParentID, SZ
+	) as ExtraStructureLinks 
+	
+	where NumPairs > 1 order by NumPairs, SParentID
 
 GO
+*/
 
 --Renders the first 1000 shapes in the spatial results view
 IF OBJECT_ID('tempdb..#GAggregate') IS NOT NULL DROP TABLE #GAggregate
@@ -74,16 +86,18 @@ insert into #GAggregate (SParentID, Shape) Select SParentID, Line from #Structur
 --select SParentID, Shape from #GAggregate where Shape is NULL OR Shape.STIsValid() = 0
 
 --select TOP 1000 SParentID, Shape as Aggregate from #GAggregate order by SParentID
-select TOP 1000 SParentID, geometry::CollectionAggregate(Shape) as Aggregate from #GAggregate group by SParentID order by SParentID
+--select TOP 1000 SParentID, geometry::CollectionAggregate(Shape) as Aggregate from #GAggregate group by SParentID order by SParentID
 
 GO
 
---Measures the area of the PSD
 declare @AreaScalar float
+--Measures the area of the PSD
 set @AreaScalar = dbo.XYScale() * dbo.ZScale()
-select S.ParentID as SourceParentID, SL.SParentID as SourceID, T.ParentID as TargetParentID, SL.TParentID as TargetID, S.TypeID as SourceTypeID, T.TypeID as TargetTypeID, sum( Line.STLength()) * @AreaScalar as Area_nm, geometry::CollectionAggregate(Shape) as shape from #StructureLinks SL
-	join #GAggregate GA ON SL.SParentID = GA.SParentID
+select S.ParentID as SourceParentID, SL.SParentID as SourceID, T.ParentID as TargetParentID, SL.TParentID as TargetID,
+	   S.TypeID as SourceTypeID, T.TypeID as TargetTypeID, sum( Line.STLength()) * @AreaScalar as Area_nm,
+	   sum( Line.STLength()) * @AreaScalar / 1000000.0 as Area_um--, geometry::CollectionAggregate(Shape) as shape 
+from #StructureLinks SL
 	join Structure S ON S.ID = SL.SParentID
 	join Structure T ON T.ID = SL.TParentID
 	group by SL.SParentID, SL.TParentID, S.ParentID, T.ParentID, S.TypeID, T.TypeID
-	order by Area_nm
+	order by TargetTypeID
