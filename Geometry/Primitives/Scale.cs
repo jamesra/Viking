fc7337a8eb37d38f633a9ec94ec1c71dd3a05464
James Anderson <james.r.andreson@utah.edu>
2016-10-06 17:36:16 -0700

    Added file, moved from AnnotationVizLib library

---
+++ Geometry/Primitives/Scale.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Geometry
+{ 
+    [Serializable]
+    public class AxisUnits
+    {
+        public double Value { get; private set; }
+        public string Units { get; private set; }
+
+        public AxisUnits(double value, string units)
+        {
+            Value = value;
+            Units = units; 
+        }
+    }
+
+    [Serializable]
+    public class Scale
+    {
+        public AxisUnits X { get; private set; }
+        public AxisUnits Y { get; private set; }
+        public AxisUnits Z { get; private set; }
+
+        public Scale(AxisUnits X, AxisUnits Y, AxisUnits Z)
+        {
+            this.X = X;
+            this.Y = Y;
+            this.Z = Z; 
+        }
+    }
+}
