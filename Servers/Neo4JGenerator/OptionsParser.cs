5f3dfe805cafebfb867d1bb47da0311f7a5d270f
James Anderson <james.r.anderson@utah.edu>
2017-05-25 18:04:34 -0600

    Initial commit of Neo4JGenerator console application.

---
+++ Servers/Neo4JGenerator/OptionsParser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+using CommandLine.Text;
+
+namespace Neo4JGenerator
+{
+
+    class CommandLineOptions
+    {
+        [Option('u', "URL", Required = true, HelpText = "URL of JSON Exporter")]
+        public string JsonURL { get; set; }
+
+        [Option('s', "Neo4JDatabase", Required =true, HelpText ="Neo4J Database to update/create")]
+        public string Neo4JDatabase { get; set; }
+
+        [Option('u', "username", DefaultValue = "Anonymous", Required = false, HelpText = "URL of VolumeXML file")]
+        public string Username { get; set; }
+
+        [Option('p', "password", DefaultValue = "connectome", Required = false, HelpText = "URL of VolumeXML file")]
+        public string Password { get; set; }
+        
+        [ParserState]
+        public IParserState LastParsertState { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            return CommandLine.Text.HelpText.AutoBuild(this,
+                (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+
+    }
+}
---
+++ Servers/Neo4JGenerator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Neo4j.Driver.V1;
+using System.Web;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Converters;
+
+namespace Neo4JGenerator
+{
+    class Program
+    {
+        static CommandLineOptions options = new CommandLineOptions();
+
+        static void Main(string[] args)
+        {
+            if (!CommandLine.Parser.Default.ParseArguments(args, Program.options))
+            {
+                System.Console.WriteLine("Unable to parse command line arguments, aborting");
+                return;
+            }
+
+            using (var driver = GraphDatabase.Driver("bolt://localhost:7687", AuthTokens.Basic("neo4j", "neo4j")))
+            using (var session = driver.Session())
+            {
+                session.Run("CREATE (a:Person {name: {name}, title: {title}})",
+                            new Dictionary<string, object> { { "name", "Arthur" }, { "title", "King" } });
+
+                var result = session.Run("MATCH (a:Person) WHERE a.name = {name} " +
+                                         "RETURN a.name AS name, a.title AS title",
+                                         new Dictionary<string, object> { { "name", "Arthur" } });
+
+                foreach (var record in result)
+                {
+                    Console.WriteLine($"{record["title"].As<string>()} {record["name"].As<string>()}");
+                }
+            }
+
+            Console.WriteLine("All done!");
+        }
+    }
+}
