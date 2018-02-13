e10bd3d454cea2e769077830c49738b57165cde3
James Anderson <james.r.anderson@utah.edu>
2018-02-13 15:01:26 -0700

    Updated to use the StoredProcedures for getting network spatial data via OData service.

--- AnnotationVizLibSimpleODataClient/SimpleODataSpatialDataFactory.cs
+++ AnnotationVizLibSimpleODataClient/SimpleODataSpatialDataFactory.cs
@@ -11,90 +11,36 @@ namespace AnnotationVizLib.SimpleODataClient
 {
     public class SimpleODataSpatialDataFactory
     {
-        public static void AppendSpatialDataFromOData(NeuronGraph graph, Uri Endpoint)
+
+        public static void AppendSpatialDataFromOData(NeuronGraph graph, Uri Endpoint, ICollection<long> IDs, uint Hops)
         {
             ODataClientSettings s = new ODataClientSettings();
             Simple.OData.Client.ODataClient client = new Simple.OData.Client.ODataClient(Endpoint);
             var scale = client.GetScale();
-            
-            System.Threading.Tasks.Parallel.ForEach(graph.Nodes.Values, node =>
+
+            AppendNeuronSpatialData(graph, client, IDs, Hops);
+            AppendAreaToConnections(graph, client, IDs, Hops);
+        }
+
+        public static void AppendNeuronSpatialData(NeuronGraph graph, ODataClient client, ICollection<long> IDs, uint Hops)
+        {
+            var annotations = new ODataFeedAnnotations();
+            string queryString = string.Format("NetworkSpatialData(IDs={0},Hops={1})", IDs.ToODataArrayParameterString(), Hops);
+            Task<IEnumerable<IDictionary<string, object>>> taskStructureDicts = client.FindEntriesAsync(queryString);
+            Debug.Assert(taskStructureDicts != null);
+            taskStructureDicts.Wait();
+            IEnumerable<IDictionary<string, object>> StructureDicts = taskStructureDicts.Result;
+             
+            AppendDictionaryToAttributes(graph, StructureDicts);
+
+            while (annotations.NextPageLink != null)
             {
-                try
-                {
-                    Simple.OData.Client.ODataClient taskclient = new Simple.OData.Client.ODataClient(Endpoint);
-                    AppendAreaToConnections(node, taskclient);
-                    AppendNeuronSpatialData(node, taskclient);
-                }
-                catch(System.AggregateException e)
-                {
-                    System.Diagnostics.Trace.WriteLine(string.Format("OData spatial cache Aggregate exception StructureID:{0}\n{1}", node.Key, e.ToString()));
-                    if (e.InnerException as Simple.OData.Client.WebRequestException == null)
-                    {
-                        throw e;
-                    }
-
-                    Simple.OData.Client.WebRequestException inner = e.InnerException as Simple.OData.Client.WebRequestException;
-                    if (inner.Code == System.Net.HttpStatusCode.NotFound)
-                        return;
-                }
-                catch(Simple.OData.Client.WebRequestException e)
-                {
-                    System.Diagnostics.Trace.WriteLine(string.Format("OData spatial cache exception StructureID:{0}", node.Key));
-                    if (e.Code == System.Net.HttpStatusCode.NotFound)
-                        return;
-
-                    throw e;
-
-                }
-            });
-            /*
-            foreach (NeuronNode node in graph.Nodes.Values)
-            {
-                try
-                {
-                    AppendAreaToConnections(node, client);
-                    AppendNeuronSpatialData(node, client);
-                }
-                catch(System.AggregateException e)
-                {
-                    System.Diagnostics.Trace.WriteLine(string.Format("OData spatial cache Aggregate exception StructureID:{0}\n{1}", node.Key, e.ToString()));
-                    if (e.InnerException as Simple.OData.Client.WebRequestException == null)
-                    {
-                        throw e;
-                    }
-
-                    Simple.OData.Client.WebRequestException inner = e.InnerException as Simple.OData.Client.WebRequestException;
-                    if (inner.Code == System.Net.HttpStatusCode.NotFound)
-                        continue;
-                }
-                catch(Simple.OData.Client.WebRequestException e)
-                {
-                    System.Diagnostics.Trace.WriteLine(string.Format("OData spatial cache exception StructureID:{0}", node.Key));
-                    if (e.Code == System.Net.HttpStatusCode.NotFound)
-                        continue;
-
-                }
-                catch(System.AggregateException e)
-                {
-                    System.Diagnostics.Trace.WriteLine(string.Format("OData spatial cache Aggregate exception StructureID:{0}\n{1}", node.Key, e.ToString()));
-                    if (e.InnerException as Simple.OData.Client.WebRequestException == null)
-                    {
-                        throw e;
-                    }
-
-                    Simple.OData.Client.WebRequestException inner = e.InnerException as Simple.OData.Client.WebRequestException;
-                    if (inner.Code == System.Net.HttpStatusCode.NotFound)
-                        continue;
-                }
-                catch(Simple.OData.Client.WebRequestException e)
-                {
-                    System.Diagnostics.Trace.WriteLine(string.Format("OData spatial cache exception StructureID:{0}", node.Key));
-                    if (e.Code == System.Net.HttpStatusCode.NotFound)
-                        continue;
-
-                }
-            } */
-            
+                taskStructureDicts = client.FindEntriesAsync(annotations.NextPageLink.ToString(), annotations);
+                taskStructureDicts.Wait();
+                StructureDicts = taskStructureDicts.Result;
+                 
+                AppendDictionaryToAttributes(graph, StructureDicts);
+            }
         }
 
         public static void AppendNeuronSpatialData(NeuronNode node, ODataClient client)
@@ -107,6 +53,24 @@ namespace AnnotationVizLib.SimpleODataClient
             IDictionary<string, object> StructuresDicts = taskStructureDicts.Result;
 
             AppendDictionaryToAttributes(node, StructuresDicts);
+        }
+
+        private static void AppendDictionaryToAttributes(NeuronGraph graph, IEnumerable<IDictionary<string, object>> StructureDicts)
+        {
+            foreach (IDictionary<string, object> dict in StructureDicts)
+            {
+                if (!dict.ContainsKey("ID"))
+                {
+                    continue;
+                }
+
+                long ID = System.Convert.ToInt64(dict["ID"]);
+
+                if (graph.Nodes.ContainsKey(ID))
+                {
+                    AppendDictionaryToAttributes(graph.Nodes[ID], dict);
+                }
+            }
         }
 
         private static void AppendDictionaryToAttributes(NeuronNode node, IDictionary<string, object> data)
@@ -122,75 +86,158 @@ namespace AnnotationVizLib.SimpleODataClient
             }
         }
 
+        private static Dictionary<ulong, long> BuildChildToParentMap(NeuronGraph graph)
+        {
+            Dictionary<ulong, long> ChildToParent = new Dictionary<ulong, long>();
+            foreach (NeuronNode node in graph.Nodes.Values)
+            {
+                foreach(ulong childID in node.EdgeSourceChildStructureIDs)
+                {
+                    if(!ChildToParent.ContainsKey(childID))
+                        ChildToParent.Add(childID, node.Key);
+                }
 
-        public static void AppendAreaToConnections(NeuronNode node, ODataClient client)
+                foreach (ulong childID in node.EdgeTargetChildStructureIDs)
+                {
+                    if (!ChildToParent.ContainsKey(childID))
+                        ChildToParent.Add(childID, node.Key);
+                }
+            }
+
+            return ChildToParent;
+        }
+
+        
+        private static Dictionary<ulong, SortedSet<NeuronEdge>> BuildChildToEdgeMap(NeuronGraph graph)
+        {
+            Dictionary<ulong, SortedSet<NeuronEdge>> IDToEdge = new Dictionary<ulong, SortedSet<NeuronEdge>>();
+            foreach(NeuronEdge e in graph.Edges.Values)
+            { 
+                foreach(ulong SourceID in e.SourceIDs)
+                {
+                    AddEdge(IDToEdge, SourceID, e);
+                }
+
+                foreach (ulong TargetID in e.TargetIDs)
+                {
+                    AddEdge(IDToEdge, TargetID, e);
+                }
+            }
+
+            return IDToEdge;
+        } 
+
+        private static void AddEdge(Dictionary<ulong, SortedSet<NeuronEdge>> dict, ulong ChildID, NeuronEdge value)
+        {
+            if(!dict.ContainsKey(ChildID))
+            {
+                dict.Add(ChildID, new SortedSet<NeuronEdge>());
+            }
+
+            dict[ChildID].Add(value);
+        }
+
+        public static void AppendAreaToConnections(NeuronGraph graph, ODataClient client, ICollection<long> IDs, uint Hops)
         {
             var annotations = new ODataFeedAnnotations();
-            //string queryString = string.Format("StructureSpatialCaches?$filter=ParentID eq {0}&$select=ID,Area", node.Key);
 
-            //string queryString = string.Format("Structures{0}/Children?$expand=StructureSpatialCache&$select=ID,Area", node.Key);
-            string queryString = string.Format("Structures?$filter=ParentID eq {0}&$select=StructureSpatialCache&$expand=StructureSpatialCache&($select=Area,Volume)", node.Key);
-            Task <IEnumerable<IDictionary<string, object>>> taskStructureDicts = client.FindEntriesAsync(queryString, annotations);
+            Dictionary<ulong, SortedSet<NeuronEdge>> IDToEdge = BuildChildToEdgeMap(graph);
+            Dictionary<ulong, long> ChildToParent = BuildChildToParentMap(graph);
+            
+            string queryString = string.Format("NetworkEdgeSpatialData(IDs={0},Hops={1})", IDs.ToODataArrayParameterString(), Hops);
+            Task<IEnumerable<IDictionary<string, object>>> taskStructureDicts = client.FindEntriesAsync(queryString, annotations);
             Debug.Assert(taskStructureDicts != null);
             taskStructureDicts.Wait();
-            IEnumerable<IDictionary<string, object>> StructuresDicts = taskStructureDicts.Result.Where(r => r.ContainsKey("StructureSpatialCache")).Select(r => r["StructureSpatialCache"] as Dictionary<string, object>);
 
-            AppendNeuronEdgeArea(node, StructuresDicts);
+            IEnumerable<IDictionary<string, object>> StructureEdgeDicts = taskStructureDicts.Result;
+             
+            AddSpatialDataToEdges(graph, StructureEdgeDicts, IDToEdge);
+            
 
             while (annotations.NextPageLink != null)
             {
                 taskStructureDicts = client.FindEntriesAsync(annotations.NextPageLink.ToString(), annotations);
                 taskStructureDicts.Wait();
-                StructuresDicts = taskStructureDicts.Result.Where(r => r.ContainsKey("StructureSpatialCache")).Select(r => r["StructureSpatialCache"] as Dictionary<string, object>);
+                StructureEdgeDicts = taskStructureDicts.Result;
 
-                AppendNeuronEdgeArea(node, StructuresDicts);
+                AddSpatialDataToEdges(graph, StructureEdgeDicts, IDToEdge);
             }
         }
 
-        private static void AppendNeuronEdgeArea(NeuronNode node, IEnumerable<IDictionary<string, object>> nodeData)
+        public static void AddSpatialDataToEdges(NeuronGraph graph, IEnumerable<IDictionary<string, object>> EdgeStructureDicts, Dictionary<ulong, SortedSet<NeuronEdge>> IDToEdge)
         {
-            
-            foreach (IDictionary<string, object> dict in nodeData)
+            foreach(IDictionary<string, object> childData in EdgeStructureDicts)
             {
+                AddAreaToEdges(graph, childData, IDToEdge);
+                AddZToEdges(graph, childData, IDToEdge);
+            }
+        }
 
-                Debug.Assert(dict.ContainsKey("ID"));
-                if (!dict.ContainsKey("ID"))
-                    continue;
+        public static void AddAreaToEdges(NeuronGraph graph, IDictionary<string, object> childData, Dictionary<ulong, SortedSet<NeuronEdge>> IDToEdge)
+        {
+            Debug.Assert(childData.ContainsKey("ID"));
+            if (!childData.ContainsKey("ID"))
+                return;
 
-                ulong EdgeStructureID = System.Convert.ToUInt64(dict["ID"]);
+            Debug.Assert(childData.ContainsKey("Area"));
+            if (!childData.ContainsKey("Area"))
+            {
+                return;
+            }
 
-                Debug.Assert(dict.ContainsKey("Area"));
-                if (!dict.ContainsKey("Area"))
-                {
-                    continue;
-                }
+            ulong ChildKey = System.Convert.ToUInt64(childData["ID"]);
+            double Area = System.Convert.ToDouble(childData["Area"]);
 
-                double Area = (double)dict["Area"];
+            if (!IDToEdge.ContainsKey(ChildKey))
+                return;
 
-                foreach (string key in dict.Keys)
-                {
-                    foreach(SortedSet<NeuronEdge> edges in node.Edges.Values)
-                    {
-                        foreach (NeuronEdge edge in edges)
-                        {
-                            if (edge.Links.Any(e => e.SourceID == EdgeStructureID))
-                            {
-                                if (!edge.Attributes.ContainsKey("TotalSourceArea"))
-                                    edge.Attributes.Add("TotalSourceArea", 0);
+            SortedSet<NeuronEdge> edges = IDToEdge[ChildKey];
 
-                                edge.Attributes["TotalSourceArea"] = System.Convert.ToDouble(edge.Attributes["TotalSourceArea"]) + Area;
-                            }
+            foreach (NeuronEdge edge in edges.Where(e => e.SourceIDs.Contains(ChildKey)))
+            {
+                edge.TotalSourceArea += Area;
+            }
 
-                            if (edge.Links.Any(e => e.TargetID == EdgeStructureID))
-                            {
-                                if (!edge.Attributes.ContainsKey("TotalTargetArea"))
-                                    edge.Attributes.Add("TotalTargetArea", 0);
+            foreach (NeuronEdge edge in edges.Where(e => e.TargetIDs.Contains(ChildKey)))
+            {
+                edge.TotalTargetArea += Area;
+            }
+        }
 
-                                edge.Attributes["TotalTargetArea"] = System.Convert.ToDouble(edge.Attributes["TotalTargetArea"]) + Area;
-                            }
-                        }
-                    }
-                }
+        public static void AddZToEdges(NeuronGraph graph, IDictionary<string, object> childData, Dictionary<ulong, SortedSet<NeuronEdge>> IDToEdge)
+        {
+            Debug.Assert(childData.ContainsKey("ID"));
+            if (!childData.ContainsKey("ID"))
+                return;
+
+            Debug.Assert(childData.ContainsKey("MinZ"));
+            if (!childData.ContainsKey("MinZ"))
+            {
+                return;
+            }
+
+            Debug.Assert(childData.ContainsKey("MaxZ"));
+            if (!childData.ContainsKey("MaxZ"))
+            {
+                return;
+            }
+
+            ulong ChildKey = System.Convert.ToUInt64(childData["ID"]);
+            double MinZ = System.Convert.ToDouble(childData["MinZ"]);
+            double MaxZ = System.Convert.ToDouble(childData["MaxZ"]);
+
+            if (!IDToEdge.ContainsKey(ChildKey))
+                return;
+
+            SortedSet<NeuronEdge> edges = IDToEdge[ChildKey];
+
+            foreach (NeuronEdge edge in edges.Where(e => e.SourceIDs.Contains(ChildKey)))
+            {
+                if (edge.MinZ > MinZ)
+                    edge.MinZ = MinZ;
+
+                if (edge.MaxZ < MaxZ)
+                    edge.MaxZ = MaxZ;
             }
         }
     }
