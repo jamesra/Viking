876292f435de3e1abb9e1fb1c2e9727347710b5b
James Anderson <james.r.anderson@utah.edu>
2018-02-13 18:55:34 -0700

    Added helper functions for writing JSON files.

---
+++ AnnotationVizLib/Graph/JSONExtensions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace AnnotationVizLib
+{
+    static class NewtonsoftJSONExtensions
+    {
+        public static void AddAttributes(this JObject obj, IDictionary<string, object> attribs)
+        {
+            foreach (string key in attribs.Keys)
+            {
+                object value = attribs[key];
+                JToken token;
+                if (value as JToken != null)
+                {
+                    token = (JToken)value;
+                }
+                else
+                {
+                    token = JToken.FromObject(value);
+                }
+
+                obj[key] = token;
+            }
+        }
+
+        public static JArray ToJArray(this IEnumerable<IStructure> structs)
+        {
+            JArray arr = new JArray();
+            foreach(IStructure s in structs)
+            {
+                JObject obj = s.ToJObject();
+                arr.Add(obj);
+            }
+
+            return arr;
+        }
+
+        public static JObject ToJObject(this IStructure s)
+        {
+            dynamic obj = new JObject();
+            obj.ID = s.ID;
+            obj.Label = s.Label;
+            obj.ParentID = s.ParentID;
+            obj.Tags = s.TagsXML;
+            obj.TypeID = s.TypeID;
+            
+            return obj;
+        }
+    } 
+}
