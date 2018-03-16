4cdc7a16dde6bffbfcd1b90f9d768c3c95a15a60
James Anderson <james.r.andreson@utah.edu>
2018-03-15 19:29:21 -0700

    Added a new class for manipulating the 2D camera and displaying sets of polygons

---
+++ Clients/MonogameTestbed/Cursor2DCameraManipulator.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using VikingXNAGraphics;
+using VikingXNA;
+
+namespace MonogameTestbed
+{
+    class Cursor2DCameraManipulator
+    {
+        DateTime? RightThumbstickStartTime = null;
+
+        GamePadStateTracker Gamepad = null;
+
+        public float CameraTranslateSensitivity = 3.0f;
+
+        double RightThumbstickTimeScalar
+        {
+            get
+            {
+                if (!RightThumbstickStartTime.HasValue)
+                    return 1.0;
+
+                double elapsed = (DateTime.UtcNow - RightThumbstickStartTime.Value).Seconds;
+
+                if(elapsed > 5.0)
+                {
+                    elapsed = 5.0;
+                }
+                else if(elapsed <= 1.0)
+                {
+                    elapsed = 1.0;
+                }
+
+                return elapsed;
+            }
+        }
+
+        public void Update(Camera Camera)
+        {
+            GamePadState state = GamePad.GetState(PlayerIndex.One);
+
+            if (Gamepad == null)
+            {
+                Gamepad = new MonogameTestbed.GamePadStateTracker();
+                Gamepad.Update(state);
+            }
+            
+            if (state.ThumbSticks.Right != Vector2.Zero)
+            {
+                if(!RightThumbstickStartTime.HasValue)
+                {
+                    RightThumbstickStartTime = DateTime.UtcNow;
+                }
+
+                Vector2 RightStick = state.ThumbSticks.Right;
+                RightStick.X *= CameraTranslateSensitivity;
+                RightStick.Y *= CameraTranslateSensitivity;
+                Vector2 offset = new Vector2(RightStick.X * RightStick.X, RightStick.Y * RightStick.Y);
+
+                offset.X = RightStick.X < 0 ? -offset.X : offset.X;
+                offset.Y = RightStick.Y < 0 ? -offset.Y : offset.Y;
+                
+                double scalar = RightThumbstickTimeScalar;
+                if(Camera.Downsample > 1.0)
+                    scalar *= Camera.Downsample;
+
+                offset = new Vector2((float)(offset.X * scalar), (float)(offset.Y * scalar));
+                Camera.LookAt += offset;
+            }
+            else
+            {
+                RightThumbstickStartTime = null;
+            }
+
+            if (state.Triggers.Left > 0)
+            {
+                Camera.Downsample *= 1.0 - (state.Triggers.Left / 10);
+
+                if (Camera.Downsample <= 0.1)
+                {
+                    Camera.Downsample = 0.1;
+                }
+            }
+
+            if (state.Triggers.Right > 0)
+            {
+                Camera.Downsample *= 1.0 + (state.Triggers.Right / 10);
+
+                if (Camera.Downsample >= 100)
+                {
+                    Camera.Downsample = 100;
+                }
+            }
+
+            if (Gamepad.RightStick_Clicked)
+            {
+                Camera.Downsample = 1;
+                Camera.LookAt = Vector2.Zero;
+            }
+        }
+    }
+}
---
+++ Clients/MonogameTestbed/PolygonSetView.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VikingXNAGraphics;
+using VikingXNA; 
+using Geometry;
+using Microsoft.Xna.Framework;
+
+namespace MonogameTestbed
+{
+    class PolygonSetView
+    {
+        private PointSetView[] PolyPointsView = null;
+        private LineView[] PolyRingViews = null;
+
+        private List<GridPolygon> _Polygons = new List<GridPolygon>();
+
+        public Color[] PolyLineColors;
+        public Color[] PolyVertexColors;
+
+        public bool LabelIndex
+        {
+            get
+            {
+                return PolyPointsView[0].LabelIndex;
+            }
+            set
+            {
+                foreach (PointSetView psv in PolyPointsView)
+                {
+                    psv.LabelIndex = value;
+                }
+            }
+        }
+
+        public bool LabelPosition
+        {
+            get
+            {
+                return PolyPointsView[0].LabelPosition;
+            }
+            set
+            {
+                foreach (PointSetView psv in PolyPointsView)
+                {
+                    psv.LabelPosition = value;
+                }
+            }
+        }
+
+        public PolygonSetView(IEnumerable<GridPolygon> polys)
+        {
+            _Polygons = polys.ToList();
+            PolyLineColors = polys.Select(p => Color.Black.Random()).ToArray();
+            PolyVertexColors = PolyLineColors.Select(c => c.SetAlpha(0.5f)).ToArray();
+
+            UpdatePolyViews();
+        }
+
+        private void UpdatePolyViews()
+        {
+            List<PointSetView> listPointSetView = new List<PointSetView>();
+
+            List<LineView> polyRingViews = new List<LineView>();
+
+            for (int iPoly = 0; iPoly < _Polygons.Count; iPoly++)
+            {
+                GridPolygon p = _Polygons[iPoly];
+                PointSetView psv = new PointSetView();
+
+                List<GridVector2> points = p.ExteriorRing.ToList();
+                foreach (GridPolygon innerPoly in p.InteriorPolygons)
+                {
+                    points.AddRange(innerPoly.ExteriorRing);
+                }
+
+                psv.Points = points;
+
+                psv.Color = PolyVertexColors[iPoly];
+                psv.LabelIndex = false;
+
+                psv.UpdateViews();
+                listPointSetView.Add(psv);
+
+                polyRingViews.AddRange(p.AllSegments.Select(s => new LineView(s, 1, PolyLineColors[iPoly], LineStyle.Standard, false)));
+            }
+
+            PolyPointsView = listPointSetView.ToArray();
+            PolyRingViews = PolyRingViews.ToArray();
+        }
+
+        public void Draw(MonoTestbed window, Scene scene)
+        {
+            if (PolyRingViews != null)
+            {
+                LineView.Draw(window.GraphicsDevice, window.Scene, window.lineManager, PolyRingViews);
+            }
+
+            if (PolyPointsView != null)
+            {
+                foreach (PointSetView psv in PolyPointsView)
+                {
+                    psv.Draw(window, scene);
+                }
+            }
+        }
+    }
+}
