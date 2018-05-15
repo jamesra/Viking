5a28b9ea8f1c5d7507848cfe8a5809ff37414fcb
James Anderson <james.r.andreson@utah.edu>
2018-05-15 16:49:30 -0700

    Removed the default content from the Visual Studio project generator

--- Servers/IdentityServer/IdentityServer/Controllers/HomeController.cs
+++ Servers/IdentityServer/IdentityServer/Controllers/HomeController.cs
@@ -17,14 +17,14 @@ namespace IdentityServer.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "";
 
             return View();
         }
