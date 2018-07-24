02fb8ebd5d9e30dad380040e17bab2a2830e89ae
James Anderson <james.r.andreson@utah.edu>
2018-07-24 11:35:19 -0700

    Completed utility SQL script to remove StructureLinks that involve cells

--- Servers/AnnotationService/SQL/RepairScripts/DeleteStructureLinksBetweenCells.sql
+++ Servers/AnnotationService/SQL/RepairScripts/DeleteStructureLinksBetweenCells.sql
@@ -1,5 +1,9 @@
-select SS.Label as SourceLabel, TS.Label as TargetLabel, SL.* from StructureLink SL
+select SS.Label as SourceLabel, SS.TypeID as SourceType, TS.Label as TargetLabel, TS.TypeID as TargetType, SL.* from StructureLink SL
 INNER JOIN Structure SS ON SS.ID = SL.SourceID
 INNER JOIN Structure TS ON TS.ID = SL.TargetID
 WHERE SS.ParentID IS NULL OR TS.ParentID IS NULL
 
+delete SL from StructureLink SL
+INNER JOIN Structure SS ON SS.ID = SL.SourceID
+INNER JOIN Structure TS ON TS.ID = SL.TargetID
+WHERE SS.ParentID IS NULL OR TS.ParentID IS NULL
