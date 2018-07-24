bafc53b3d5b3a0cb7aa8569265f5caa9be7a061e
James Anderson <james.r.andreson@utah.edu>
2018-07-24 12:01:53 -0700

    Initial commit of testing related projects for Identity server

---
+++ Servers/IdentityServer/DevTestAPI/Controllers/IdentityController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace DevTestAPI.Controllers
+{
+    [Route("identity")]
+    [Authorize]
+    public class IdentityController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        
+        public IActionResult Get()
+        {
+            return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        }
+    }
+}
