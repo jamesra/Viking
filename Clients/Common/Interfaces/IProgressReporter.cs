43bce991920cbcb342af87bba1c8d95be36d2926
James Anderson <james.r.anderson@utah.edu>
2018-12-19 17:23:37 -0800

    Added missing interface

---
+++ Clients/Common/Interfaces/IProgressReporter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Viking.Common
+{
+    /// <summary>
+    /// Summary description for Class1
+    /// </summary>
+    public interface IProgressReporter
+    {
+        void ReportProgress(double progress, string message);
+    }
+}
