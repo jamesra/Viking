b80eaa662c5b8d3a14c487353e54fce3ec9e22d6
James Anderson <james.r.anderson@utah.edu>
2019-04-04 13:59:29 -0700

    Added migrations

---
+++ Servers/IdentityServer/IdentityServer/Data/Migrations/20190401224756_RenameOrganizationsToGroups.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace IdentityServer.Data.Migrations
+{
+    public partial class RenameOrganizationsToGroups : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(name: "Organization", schema: "dbo", newName: "Group", newSchema: "dbo");
+            migrationBuilder.RenameTable(name: "OrganizationAssignments", schema: "dbo", newName: "GroupAssignments", newSchema: "dbo");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(name: "Group", schema: "dbo", newName: "Organization", newSchema: "dbo");
+            migrationBuilder.RenameTable(name: "GroupAssignments", schema: "dbo", newName: "OrganizationAssignments", newSchema: "dbo");
+        }
+    }
+}
