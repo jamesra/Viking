3b2dc12c28dda9a06d401f97f66d114aa59b69a6
James Anderson <james.r.anderson@utah.edu>
2019-04-04 14:00:40 -0700

    Added missing MIConvexHullExtensions

---
+++ MIConvexHullExtensions/MIVector2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Geometry;
+using MIConvexHull;
+
+namespace MIConvexHullExtensions
+{
+    public struct MIVector2 :  MIConvexHull.IVertex
+    {
+        public readonly Geometry.GridVector2 P;
+        public readonly PointIndex PolyIndex;
+
+
+        public MIVector2(GridVector2 p, PointIndex index)
+        {
+            this.P = p;
+            this.PolyIndex = index;
+        }
+
+        double[] IVertex.Position
+        {
+            get
+            {
+                return new double[] { P.X, P.Y };
+            }
+        }
+    }
+}
---
+++ MIConvexHullExtensions/MIVector3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Geometry;
+using MIConvexHull;
+
+namespace MIConvexHullExtensions
+{
+    public struct MIVector3 :  MIConvexHull.IVertex
+    {
+        public readonly Geometry.GridVector3 P;
+        public readonly PointIndex PolyIndex;
+
+
+        public MIVector3(GridVector3 p, PointIndex index)
+        {
+            this.P = p;
+            PolyIndex = index; 
+        }
+
+        double[] IVertex.Position
+        {
+            get
+            {
+                return P.coords;
+            }
+        }
+
+        public override string ToString()
+        {
+            return P.ToString();
+        }
+    }
+}
