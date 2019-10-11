bfd2580cd0299a6a1d82e70339e51d3fc042a6ea
James Anderson <james.r.anderson@utah.edu>
2019-10-11 10:38:51 -0700

    Removed duplicate use of 'Points' attribute/property

--- Geometry/Primitives/GridPolyline.cs
+++ Geometry/Primitives/GridPolyline.cs
@@ -8,31 +8,31 @@ namespace Geometry
 {
     public class GridPolyline : IPolyLine2D
     {
-        List<IPoint2D> Points;
+        protected List<IPoint2D> _Points;
 
         public GridPolyline()
         {
-            Points = new List<Geometry.IPoint2D>();
+            _Points = new List<Geometry.IPoint2D>();
         }
 
         public GridPolyline(int capacity)
         {
-            Points = new List<Geometry.IPoint2D>(capacity);
+            _Points = new List<Geometry.IPoint2D>(capacity);
         }
 
         public GridPolyline(IEnumerable<IPoint2D> points)
         {
-            Points = new List<Geometry.IPoint2D>(points);
+            _Points = new List<Geometry.IPoint2D>(points);
         }
 
         public GridPolyline(IEnumerable<GridVector2> points)
         {
-            Points = new List<Geometry.IPoint2D>(points.Cast<IPoint2D>());
+            _Points = new List<Geometry.IPoint2D>(points.Cast<IPoint2D>());
         }
 
         public void Add(IPoint2D line)
         {
-            Points.Add(line);
+            _Points.Add(line);
         }
 
         public double Area
@@ -47,10 +47,10 @@ namespace Geometry
         {
             get
             {
-                double MinX = Points.Min(p => p.X);
-                double MaxX = Points.Max(p => p.X);
-                double MinY = Points.Min(p => p.Y);
-                double MaxY = Points.Max(p => p.Y);
+                double MinX = _Points.Min(p => p.X);
+                double MaxX = _Points.Max(p => p.X);
+                double MinY = _Points.Min(p => p.Y);
+                double MaxY = _Points.Max(p => p.Y);
 
                 return new GridRectangle(MinX, MaxX, MinY, MaxY);
             }
@@ -68,11 +68,11 @@ namespace Geometry
         {
             get
             {
-                List<ILineSegment2D> listSegments = new List<ILineSegment2D>(this.Points.Count - 1);
+                List<ILineSegment2D> listSegments = new List<ILineSegment2D>(this._Points.Count - 1);
 
-                for (int i = 0; i < Points.Count - 1; i++)
+                for (int i = 0; i < _Points.Count - 1; i++)
                 {
-                    listSegments.Add(new GridLineSegment(Points[i], Points[i + 1] ));
+                    listSegments.Add(new GridLineSegment(_Points[i], _Points[i + 1] ));
                 }
 
                 return listSegments;
@@ -83,7 +83,7 @@ namespace Geometry
         {
             get
             {
-                return this.Points;
+                return this._Points;
             }
         }
 
@@ -99,9 +99,9 @@ namespace Geometry
 
         public IShape2D Translate(IPoint2D offset)
         {
-            List<IPoint2D> translatedPoints = new List<Geometry.IPoint2D>(this.Points.Count);
+            List<IPoint2D> translatedPoints = new List<Geometry.IPoint2D>(this._Points.Count);
 
-            translatedPoints = this.Points.Select(p => new GridVector2(p.X + offset.X, p.Y + offset.Y)).Cast<IPoint2D>().ToList();
+            translatedPoints = this._Points.Select(p => new GridVector2(p.X + offset.X, p.Y + offset.Y)).Cast<IPoint2D>().ToList();
 
             return new GridPolyline(translatedPoints);
         }
