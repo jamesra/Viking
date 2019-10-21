b75fa019684bb6c0d8ef54e426b325875cc7ac6c
James Anderson <james.r.anderson@utah.edu>
2019-10-21 14:00:49 -0600

    Added a helper function to chunk a list into a set of sorted list

---
+++ Servers/AnnotationService/AnnotationService/Utils.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Annotation
+{
+    internal static class Utils
+    {
+        public static List<long[]> SortAndChunk(this ICollection<long> IDs, uint maxchunksize)
+        {
+            List<long> list_IDs = IDs.ToList();
+            list_IDs.Sort();
+            int numChunks = (int)Math.Ceiling((float)IDs.Count / (float)maxchunksize);
+            int chunk_size = (int)Math.Ceiling((float)IDs.Count / numChunks);
+
+            List<long[]> output = new List<long[]>(numChunks);
+
+            while (list_IDs.Count > 0)
+            {
+                int NumIDs = list_IDs.Count < chunk_size ? list_IDs.Count : chunk_size;
+
+                long[] ShorterIDArray = new long[NumIDs];
+
+                list_IDs.CopyTo(0, ShorterIDArray, 0, NumIDs);
+                list_IDs.RemoveRange(0, NumIDs);
+
+                //yield(ShorterIDArray);
+                output.Add(ShorterIDArray);
+            }
+
+            return output;
+        }
+    }
+}
