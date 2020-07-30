b7202f2e583e8357e259d8fbbfea7b2f8e60828d
James Anderson <james.r.anderson@utah.edu>
2020-07-30 15:44:48 -0700

    Added build improvements for content and IHitTesting interface

---
+++ Clients/MonogameXNAGraphicsShared/Views/Interfaces/IHitTesting.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Geometry;
+
+namespace VikingXNAGraphics
+{
+    public interface IHitTesting
+    { 
+        /// <summary>
+        /// The bounding box of the region we are interested in hit testing
+        /// </summary>
+        GridRectangle BoundingBox { get; }
+
+        /// <summary>
+        /// True if the passed point falls inside the view, a hit-testing function
+        /// </summary>
+        /// <param name="Position"></param>
+        /// <returns></returns>
+        bool Contains(GridVector2 Position);
+    }
+}
