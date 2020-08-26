a0ce76e2cc4eb5f64a4110a3883c4ff362fed4dd
James Anderson <james.r.anderson@utah.edu>
2020-08-26 15:51:37 -0700

    Added interface for IPermittedStructureLink

---
+++ AnnotationInterfaces/IPermittedStructureLink.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Annotation.Interfaces
+{
+    public interface IPermittedStructureLink : IEquatable<IPermittedStructureLink>
+    {
+        ulong SourceTypeID { get; }
+        ulong TargetTypeID { get; }
+        bool Directional { get; }
+    }
+}
