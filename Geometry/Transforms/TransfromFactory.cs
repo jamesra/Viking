eaeb5a51ad3f76cc81e7a69aed5d418c77309875
James Anderson <James.r.anderson@utah.edu>
2021-10-26 12:53:53 -0700

    Use configureawait(false) for transform parsing

--- Geometry/Transforms/TransfromFactory.cs
+++ Geometry/Transforms/TransfromFactory.cs
@@ -120,7 +120,7 @@ namespace Geometry.Transforms
 
             using (Stream transformStream = File.OpenRead(stosfile))
             {
-                ITransform transform = await ParseStos(transformStream, Info, pixelSpacing); 
+                ITransform transform = await ParseStos(transformStream, Info, pixelSpacing).ConfigureAwait(false);
                 return transform;
             }
 
@@ -168,7 +168,7 @@ namespace Geometry.Transforms
 
         public static async Task<ITransform> ParseStos(Stream stream, StosTransformInfo info, int pixelSpacing)
         {
-            string[] lines = await StreamUtil.StreamToLines(stream);
+            string[] lines = await StreamUtil.StreamToLines(stream).ConfigureAwait(false); ;
             string[] controlDims = lines[4].Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
             string[] mappedDims = lines[5].Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
               
