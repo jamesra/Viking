c07ed40fd54024d7d36ed15044a51e5cf9132d7e
James Anderson <James.r.anderson@utah.edu>
2021-11-05 13:28:24 -0700

    Moved TextureData to a seperate file.

---
+++ Clients/Viking/NGVV/Threading/TextureData.cs
@@ -0,0 +1,22 @@
+﻿namespace Viking
+{
+    /// <summary>
+    /// Holder for texture data created on threads so that textures are created only on the main thread
+    /// XNA's behavior for multi-threaded GPU use is ambiguous
+    /// </summary>
+    internal readonly struct TextureData
+    {
+        public readonly byte[] pixelBytes;
+        public readonly int width;
+        public readonly int height;
+
+        public bool IsEmpty => pixelBytes == null;
+        
+        public TextureData(byte[] data, int width, int height)
+        {
+            this.pixelBytes = data;
+            this.width = width;
+            this.height = height;
+        }
+    }
+}
