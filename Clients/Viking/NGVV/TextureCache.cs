5d2be7106f1b71dd5e5391300b0532e88b625f83
James Anderson <james.r.anderson@utah.edu>
2022-02-04 15:05:24 -0800

    Fixed error when cache path doesn't exist Pass around DirectoryInfo and FileInfo as parameters instead of strings.

--- Clients/Viking/NGVV/TextureCache.cs
+++ Clients/Viking/NGVV/TextureCache.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web.UI;
@@ -12,11 +13,15 @@ namespace Viking
     internal class LocalTextureCacheEntry : CacheEntry<string>
     {
         public LocalTextureCacheEntry(string filename)
-            : base(filename)
-        {
-            FileInfo info = new FileInfo(filename);
-            this.Size = info.Length;
-            this.LastAccessed = info.LastAccessTimeUtc;
+            : this(new FileInfo(filename))
+        { 
+        }
+
+        public LocalTextureCacheEntry(FileInfo fileinfo)
+            : base(fileinfo.FullName)
+        { 
+            this.Size = fileinfo.Length;
+            this.LastAccessed = fileinfo.LastAccessTimeUtc;
         }
 
         public override void Dispose()
@@ -58,8 +63,13 @@ namespace Viking
         {
             DateTime Start = DateTime.Now;
             Trace.WriteLine("Populating cache", "TextureUse");
-
-            await CheckDirectory(path, token);
+            var dirinfo = new DirectoryInfo(path);
+            if (false == dirinfo.Exists)
+            {
+                dirinfo.Create();
+            }
+                
+            await CheckDirectory(dirinfo, token);
 
             TimeSpan elapsed = new TimeSpan(DateTime.Now.Ticks - Start.Ticks);
             Trace.WriteLine("Finish cache populate: " + elapsed.ToString(), "TextureUse");
@@ -69,21 +79,22 @@ namespace Viking
         /// Recursively check the supplied directory and all subdirectories, adding files to cache lists
         /// </summary>
         /// <param name="path"></param>
-        private async Task CheckDirectory(string path, CancellationToken token)
+        private async Task CheckDirectory(DirectoryInfo path, CancellationToken token)
         {
-            string[] dirs = System.IO.Directory.GetDirectories(path);
-            System.Collections.Generic.List<Task> listTasks = new System.Collections.Generic.List<Task>(dirs.Length);
-            foreach (string dir in dirs)
+            if (path.Exists == false)
+                return;
+
+            var subdirs = path.EnumerateDirectories().ToArray();
+            System.Collections.Generic.List<Task> listTasks = new System.Collections.Generic.List<Task>(subdirs.Length);
+            foreach (var subdir in subdirs)
             {
                 if (token.IsCancellationRequested)
                     return;
 
-                listTasks.Add(CheckDirectory(dir, token));
+                listTasks.Add(CheckDirectory(subdir, token));
             }
-
-            string[] files = System.IO.Directory.GetFiles(path); 
-
-            foreach (string file in files)
+              
+            foreach (var file in path.EnumerateFiles())
             {
                 LocalTextureCacheEntry entry = new LocalTextureCacheEntry(file);
 
