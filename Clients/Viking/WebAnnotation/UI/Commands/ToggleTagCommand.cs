7ceb007da6ea6ba23741fdc6cd34e4851d032872
James Anderson <james.r.anderson@utah.edu>
2022-02-08 12:39:18 -0800

    Added ability to specify arbitrary value in webannotationusersettings.xml

--- Clients/Viking/WebAnnotation/UI/Commands/ToggleTagCommand.cs
+++ Clients/Viking/WebAnnotation/UI/Commands/ToggleTagCommand.cs
@@ -6,35 +6,47 @@ namespace WebAnnotation.UI.Commands
 {
     class ToggleStructureTag : Viking.UI.Commands.Command
     {
-        StructureObj target;
-        string tag;
-        bool SetValueToUsername = false;
+        readonly StructureObj target;
+        readonly string tag;
+        private readonly string value;
+
         public ToggleStructureTag(Viking.UI.Controls.SectionViewerControl parent,
                                          StructureObj structure,
-                                         string Tag, bool setValueToUsername)
+                                         string Tag, bool setValueToUsername = false)
             : base(parent)
         {
             this.target = structure;
             this.tag = Tag;
-            this.SetValueToUsername = setValueToUsername;
+            this.value = setValueToUsername ? WebAnnotationModel.State.UserCredentials.UserName : null;
+        }
+
+        public ToggleStructureTag(Viking.UI.Controls.SectionViewerControl parent,
+            StructureObj structure,
+            string Tag, string value)
+            : base(parent)
+        {
+            this.target = structure;
+            this.tag = Tag;
+            this.value = value;
         }
 
         public override void OnActivate()
         {
-            this.Parent.BeginInvoke((Action)delegate () { this.Execute(); });
+            this.Parent.BeginInvoke((Action)this.Execute);
         }
 
         protected override void Execute()
         {
-            target.ToggleAttribute(this.tag, this.SetValueToUsername ? WebAnnotationModel.State.UserCredentials.UserName : null);
+            target.ToggleAttribute(this.tag, this.value);
 
             try
             {
                 Store.Structures.Save();
             }
-            catch (FaultException)
+            catch (FaultException ex)
             {
-                target.ToggleAttribute(this.tag, this.SetValueToUsername ? WebAnnotationModel.State.UserCredentials.UserName : null);
+                AnnotationOverlay.ShowFaultExceptionMsgBox(ex);
+                target.ToggleAttribute(this.tag, this.value);
             }
 
             base.Execute();
@@ -43,35 +55,37 @@ namespace WebAnnotation.UI.Commands
 
     class ToggleLocationTag : Viking.UI.Commands.Command
     {
-        LocationObj target;
-        string tag;
-        bool SetValueToUsername = false;
+        readonly LocationObj target;
+        readonly string tag;
+        private readonly string value;
+
         public ToggleLocationTag(Viking.UI.Controls.SectionViewerControl parent,
                                          LocationObj loc,
-                                         string Tag, bool setValueToUsername)
+                                         string Tag, bool setValueToUsername = false)
             : base(parent)
         {
             this.target = loc;
             this.tag = Tag;
-            this.SetValueToUsername = setValueToUsername;
+            this.value = setValueToUsername ? WebAnnotationModel.State.UserCredentials.UserName : null;
+        }
+
+        public ToggleLocationTag(Viking.UI.Controls.SectionViewerControl parent,
+            LocationObj loc,
+            string Tag, string Value)
+            : base(parent)
+        {
+            this.target = loc;
+            this.tag = Tag;
+            this.value = Value;
         }
 
         public override void OnActivate()
         {
-            this.Parent.BeginInvoke((Action)delegate () { this.Execute(); });
+            this.Parent.BeginInvoke((Action)this.Execute);
         }
 
         protected override void Execute()
-        {
-            if (this.SetValueToUsername)
-            {
-                target.ToggleAttribute(this.tag, WebAnnotationModel.State.UserCredentials.UserName);
-            }
-            else
-            {
-                target.ToggleAttribute(this.tag);
-            }
-
+        { 
             try
             {
                 Store.Locations.Save();
@@ -79,7 +93,7 @@ namespace WebAnnotation.UI.Commands
             catch (System.ServiceModel.FaultException ex)
             {
                 AnnotationOverlay.ShowFaultExceptionMsgBox(ex);
-                target.ToggleAttribute(this.tag, SetValueToUsername ? WebAnnotationModel.State.UserCredentials.UserName : null);
+                target.ToggleAttribute(this.tag, value);
             }
 
             base.Execute();
