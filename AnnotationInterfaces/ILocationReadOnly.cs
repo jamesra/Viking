900c250be42e6e31a9c9190f8672443da71b170d
James Anderson <james.r.anderson@utah.edu>
2022-02-25 19:09:42 -0800

    Added documentation

--- AnnotationInterfaces/ILocationReadOnly.cs
+++ AnnotationInterfaces/ILocationReadOnly.cs
@@ -3,16 +3,31 @@ using System.Collections.Generic;
 
 namespace Viking.AnnotationServiceTypes.Interfaces
 {
+    /// <summary>
+    /// Represents the Type column in the SQL Locations table
+    /// </summary>
     public enum LocationType
     {
         POINT = 0,
         CIRCLE = 1,
         ELLIPSE = 2,
         POLYLINE = 3,
-        POLYGON = 4,     //Polygon, no smoothing of exterior verticies with curve fitting
-        OPENCURVE = 5,   //Line segments with a line width, additional control points created using curve fitting function
-        CURVEPOLYGON = 6, //Polygon whose outer and inner verticies are supplimented with a curve fitting function
-        CLOSEDCURVE = 7 //Ring of line segments with a line width
+        /// <summary>
+        /// Polygon, no smoothing of exterior verticies with curve fitting
+        /// </summary>
+        POLYGON = 4,     
+        /// <summary>
+        /// Line segments with a line width, additional control points created using curve fitting function
+        /// </summary>
+        OPENCURVE = 5,  
+        /// <summary>
+        /// Polygon whose outer and inner verticies are supplimented with a curve fitting function
+        /// </summary>
+        CURVEPOLYGON = 6,
+        /// <summary>
+        /// Ring of line segments with a line width
+        /// </summary>
+        CLOSEDCURVE = 7 
     };
 
     public interface ILocation : IEquatable<ILocation>
