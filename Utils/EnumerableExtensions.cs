171099079e23a966f0e13b098a163b3b19b11fac
James Anderson <james.r.anderson@utah.edu>
2022-02-26 13:41:16 -0800

    Added helper extension to print a list as csv

---
+++ Utils/EnumerableExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Viking
+{
+    public static class EnumerableExtensions
+    {
+        public static string ToCsv<T>(this IEnumerable<T> collection)
+        {
+            StringBuilder sb = new StringBuilder();
+            bool first = true;
+            foreach (T item in collection)
+            {
+                if (!first)
+                    sb.Append($", {item}");
+                else
+                {
+                    first = false;
+                    sb.Append(item.ToString());
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
