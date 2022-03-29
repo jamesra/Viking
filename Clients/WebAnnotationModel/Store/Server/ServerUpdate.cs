188caeb5f9ab27c8ee7737e7c84648ff88a13d30
James Anderson <james.r.anderson@utah.edu>
2022-03-29 16:45:44 -0700

    Added interfaces to server results

--- Clients/WebAnnotationModel/Store/Server/ServerUpdate.cs
+++ Clients/WebAnnotationModel/Store/Server/ServerUpdate.cs
@@ -1,8 +1,24 @@
 ﻿using System;
 
-namespace WebAnnotationModel
+namespace WebAnnotationModel.ServerInterface
 {
-    public readonly struct ServerUpdate<KEY, SERVER_OBJECT>
+    public interface IServerUpdate<out KEY, out SERVER_OBJECT>
+    {
+        /// <summary>
+        /// When this update was executed on the server
+        /// </summary>
+        DateTime QueryTime { get; }
+        /// <summary>
+        /// Objects returned by the server, they may be new or updates or unchanged.
+        /// </summary>
+        SERVER_OBJECT NewOrUpdated { get; }
+        /// <summary>
+        /// Objects the server is reporting deleted
+        /// </summary>
+        KEY[] DeletedIDs { get; }
+    }
+
+    public readonly struct ServerUpdate<KEY, SERVER_OBJECT> : IServerUpdate<KEY, SERVER_OBJECT>
     {
         /// <summary>
         /// When this update was performed on the server
@@ -43,5 +59,9 @@ namespace WebAnnotationModel
             NewOrUpdated = default;
             DeletedIDs = deleted ?? Array.Empty<KEY>();
         }
+
+        DateTime IServerUpdate<KEY, SERVER_OBJECT>.QueryTime => QueryTime;
+        SERVER_OBJECT IServerUpdate<KEY, SERVER_OBJECT>.NewOrUpdated => NewOrUpdated;
+        KEY[] IServerUpdate<KEY, SERVER_OBJECT>.DeletedIDs => DeletedIDs;
     }
 }
