8139e749b1e2d49eebe55cb55411596241664b04
James Anderson <james.r.anderson@utah.edu>
2023-01-24 20:16:06 -0800

    Fixed IsUpper calculation for an edge case

--- Geometry/Transforms/GridTransform.cs
+++ Geometry/Transforms/GridTransform.cs
@@ -101,16 +101,13 @@ namespace Geometry.Transforms
                 IsUpper = true;
                 iX--;
             }
-            else if (OffsetY + double.Epsilon >= Bounds.Height)
+            
+            if (OffsetY + double.Epsilon >= Bounds.Height)
             {
                 IsUpper = true;
                 iY--;
             }
-            else
-            {
-                IsUpper = (X - iX) + (Y - iY) > 1;
-            }
-
+              
             int iTri = (iY << 1) + (((GridSizeY - 1) << 1) * iX); //(iY * 2) + ((GridSizeY - 1) * 2 * iX)
             //int iTri = (iX * 2) + ((GridSizeX-1) * 2 * iY);
             iTri += IsUpper ? 1 : 0;
