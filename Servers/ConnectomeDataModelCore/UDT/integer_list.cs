2522a734abea6f746e76ac3f8a053a61c76e64c3
James Anderson <james.r.anderson@utah.edu>
2025-10-24 13:06:20 -0700

    Adding UDT Types

--- Servers/ConnectomeDataModelCore/UDT/integer_list.cs
+++ Servers/ConnectomeDataModelCore/UDT/integer_list.cs
@@ -5,13 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 using EntityFrameworkExtras;
 using EntityFrameworkExtras.EFCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Viking.DataModel.Annotation.UDT
 {
     [UserDefinedTableType("integer_list")]
+    
     public class integer_list
     {
         [UserDefinedTableTypeColumn(1, nameof(ID))]
+        [Key]
         public long ID { get; set; }
     }
 }
---
+++ Servers/ConnectomeDataModelCore/UDT/udtLinks.cs
@@ -0,0 +1,19 @@
+﻿using EntityFrameworkExtras.EFCore;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Viking.DataModel.Annotation.UDT
+{
+    [UserDefinedTableType("udtLinks")] 
+    [Index(nameof(SourceID), Name = nameof(SourceID))]
+    [Index(nameof(TargetID), Name = nameof(TargetID))]
+    public class udtLinks
+    {
+        [UserDefinedTableTypeColumn(1, nameof(SourceID))]
+        public long SourceID { get; set; }
+
+        [UserDefinedTableTypeColumn(1, nameof(TargetID))]
+        public long TargetID { get; set; }
+    }
+}
