e4744764eaca84a5d9e84e31b99ae7c9fb577dda
James Anderson <James.Anderson@pitt.edu>
2026-02-05 18:26:53 -0800

    Improved performance of SortAndChunk utility function that chunks IDs before sending them into SQL to make sure they fit under the parameter limit.

--- Servers/AnnotationService/AnnotationService/Utils.cs
+++ Servers/AnnotationService/AnnotationService/Utils.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,24 +8,40 @@ namespace Annotation
     {
         public static List<long[]> SortAndChunk(this ICollection<long> IDs, uint maxchunksize)
         {
-            List<long> list_IDs = [.. IDs];
-            list_IDs.Sort();
-            int numChunks = (int)Math.Ceiling((float)IDs.Count / (float)maxchunksize);
-            int chunk_size = (int)Math.Ceiling((float)IDs.Count / numChunks);
+            //We created a copy of the input so no need to copy it again to sort
+            return IDs.ToArray().SortAndChunk(maxchunksize, CanSortIDsInPlace: true); 
+        }
+
+        public static List<long[]> SortAndChunk(this long[] IDs, uint maxchunksize, bool CanSortIDsInPlace = false)
+        {
+            long[] sorted;
+            if(CanSortIDsInPlace)
+            { 
+                sorted = IDs;
+            }
+            else
+            {
+                sorted = new long[IDs.Length];
+                IDs.CopyTo(sorted, 0);                
+            }
+
+            int count = IDs.Length;
+            Array.Sort(sorted);
+
+            int numChunks = (int)Math.Ceiling((float)count / (float)maxchunksize);
+            int chunk_size = (int)Math.Ceiling((float)count / numChunks);
 
             List<long[]> output = new(numChunks);
 
-            while (list_IDs.Count > 0)
+            for (int i = 0; i < numChunks; i++)
             {
-                int NumIDs = list_IDs.Count < chunk_size ? list_IDs.Count : chunk_size;
-
-                long[] ShorterIDArray = new long[NumIDs];
-
-                list_IDs.CopyTo(0, ShorterIDArray, 0, NumIDs);
-                list_IDs.RemoveRange(0, NumIDs);
-
-                //yield(ShorterIDArray);
-                output.Add(ShorterIDArray);
+                int start = i * chunk_size;
+                int len = Math.Min(chunk_size, count - start);
+                if (len <= 0)
+                    continue;
+                long[] chunk = new long[len];
+                Array.Copy(sorted, start, chunk, 0, len);
+                output.Add(chunk);
             }
 
             return output;
